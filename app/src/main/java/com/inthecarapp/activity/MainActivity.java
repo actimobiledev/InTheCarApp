9abceb7cb9bdb5e2ccd49db12a7b1835621eed28
actimobiledev <actiknow.mobiledev@gmail.com>
2020-12-17 15:23:35 +0530

    initial

--- app/src/main/java/com/inthecarapp/activity/MainActivity.java
+++ app/src/main/java/com/inthecarapp/activity/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.inthecarapp.R;
-
+//rahul jain
 public class MainActivity extends AppCompatActivity {
 
     @Override
